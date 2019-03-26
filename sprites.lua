157350001b83fb8c5e2f6c4dd7659118ca34205f
xTheJakalx <jakalsden@hotmail.com>
2019-03-26 15:40:14 +1000

    Changed case of names in sprites.lua file to correct state - linux is picky about that! :D

--- sprites.lua
+++ sprites.lua
@@ -1,18 +1,18 @@
 sprites = {}
 
   --animals
-  sprites.elephant = love.graphics.newImage('sprites/elephant.png')
-  sprites.giraffe = love.graphics.newImage('sprites/giraffe.png')
-  sprites.hippo = love.graphics.newImage('sprites/hippo.png')
-  sprites.monkey = love.graphics.newImage('sprites/monkey.png')
-  sprites.panda = love.graphics.newImage('sprites/panda.png')
-  sprites.parrot = love.graphics.newImage('sprites/parrot.png')
-  sprites.penguin = love.graphics.newImage('sprites/penguin.png')
-  sprites.pig = love.graphics.newImage('sprites/pig.png')
-  sprites.rabbit = love.graphics.newImage('sprites/rabbit.png')
-  sprites.snake = love.graphics.newImage('sprites/snake.png')
+  sprites.elephant = love.graphics.newImage('Sprites/elephant.png')
+  sprites.giraffe = love.graphics.newImage('Sprites/giraffe.png')
+  sprites.hippo = love.graphics.newImage('Sprites/hippo.png')
+  sprites.monkey = love.graphics.newImage('Sprites/monkey.png')
+  sprites.panda = love.graphics.newImage('Sprites/panda.png')
+  sprites.parrot = love.graphics.newImage('Sprites/parrot.png')
+  sprites.penguin = love.graphics.newImage('Sprites/penguin.png')
+  sprites.pig = love.graphics.newImage('Sprites/pig.png')
+  sprites.rabbit = love.graphics.newImage('Sprites/rabbit.png')
+  sprites.snake = love.graphics.newImage('Sprites/snake.png')
 
-  sprites.font = love.graphics.newImageFont("sprites/imagefont.png",
+  sprites.font = love.graphics.newImageFont("Sprites/Imagefont.png",
                                             " abcdefghijklmnopqrstuvwxyz" ..
                                             "ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
                                             "123456789.,!?-+/():;%&`'*#=[]\"")
