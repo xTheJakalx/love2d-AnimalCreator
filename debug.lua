554bff0c7e25f288eaf76fa770269de49f2a25b2
xTheJakalx <jakalsden@hotmail.com>
2019-03-30 13:34:28 +1000

    Found a problem where i was stupidly spawning all the buttons in the draw function of the debug.lua file... (so it was constantly spawning thousands of buttons a second)

--- debug.lua
+++ debug.lua
@@ -13,56 +13,25 @@ local function Draw()
     yPos = yPos + 15
     love.graphics.print("Total Animals: " .. #animals, 15, yPos)
     yPos = yPos + 15
-    love.graphics.print("Save file exists? ".. (saveFile and 'true' or 'false'), 15, yPos) -- this is how you print booleans
-    yPos = yPos + 25
-    buttonSpacing = yPos
-    spawnButton(15, buttonSpacing, 155, 30, "Quit", "quit")
-    buttonSpacing = buttonSpacing + 35
-    spawnButton(15, buttonSpacing, 155, 30, "Delete", "delete")
-    buttonSpacing = buttonSpacing + 35
-    spawnButton(15, buttonSpacing, 155, 30, "Load", "load")
-    buttonSpacing = buttonSpacing + 35
-    spawnButton(15, buttonSpacing, 155, 30, "Save", "save")
-    buttonSpacing = buttonSpacing + 35
-    spawnButton(15, buttonSpacing, 155, 30, "Clear", "clear")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Elephant", "elephant")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Giraffe", "giraffe")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Hippo", "hippo")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Monkey", "monkey")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Panda", "panda")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Parrot", "parrot")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Penguin", "penguin")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Pig", "pig")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Rabbit", "rabbit")
-    buttonSpacing = buttonSpacing + 40
-    spawnButton(15, buttonSpacing, 155, 36, "Snake", "snake")
-    yPos = yPos + 20
+    -- FUN FACT: this is how you print booleans
+    love.graphics.print("Save file exists? ".. (saveFile and 'true' or 'false'), 15, yPos)
+    yPos = yPos + 50
     local animalNum = 1
-    yPos = yPos + 15
     -- place will be row
     local xPos = 350 -- where to start drawing the animal icons on the x axis
     local columnNum = 1 -- which column to draw the icon to
     for i,a in ipairs(animals) do
-       -- if more than 4 columns return to column 1
-      if columnNum > 4 then
-        columnNum = 1
-        yPos = yPos + 85
-        xPos = 350
-      end
-      -- move over to the next column
-      columnNum = columnNum + 1
-      -- add 95 pixels to column position (x)
-      xPos = xPos + 95
-      love.graphics.draw(a.sprite, xPos, yPos, nil, .25, .25, a.sprite:getWidth()/2, a.sprite:getHeight()/2)
+         -- if more than 4 columns return to column 1
+        if columnNum > 4 then
+          columnNum = 1
+          yPos = yPos + 85
+          xPos = 350
+        end
+        -- move over to the next column
+        columnNum = columnNum + 1
+        -- add 95 pixels to column position (x)
+        xPos = xPos + 95
+        love.graphics.draw(a.sprite, xPos, yPos, nil, .25, .25, a.sprite:getWidth()/2, a.sprite:getHeight()/2)
     end
     love.graphics.print("Table of Animals".. " - Total Animals: ".. #animals, 450,20)
     love.graphics.print(table.show(animals, "animals"), 750,40)
--- main.lua
+++ main.lua
@@ -15,6 +15,37 @@ function love.load()
   buttonTimer = 0
   spawnAnimal = false
   saveFile = false
+  --this was in the draw function before and was constantly creating buttons (doh!)
+  buttonSpacing = 95
+  spawnButton(15, buttonSpacing, 155, 30, "Quit", "quit")
+  buttonSpacing = buttonSpacing + 35
+  spawnButton(15, buttonSpacing, 155, 30, "Delete", "delete")
+  buttonSpacing = buttonSpacing + 35
+  spawnButton(15, buttonSpacing, 155, 30, "Load", "load")
+  buttonSpacing = buttonSpacing + 35
+  spawnButton(15, buttonSpacing, 155, 30, "Save", "save")
+  buttonSpacing = buttonSpacing + 35
+  spawnButton(15, buttonSpacing, 155, 30, "Clear", "clear")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Elephant", "elephant")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Giraffe", "giraffe")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Hippo", "hippo")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Monkey", "monkey")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Panda", "panda")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Parrot", "parrot")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Penguin", "penguin")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Pig", "pig")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Rabbit", "rabbit")
+  buttonSpacing = buttonSpacing + 40
+  spawnButton(15, buttonSpacing, 155, 36, "Snake", "snake")
 end
 
 function love.update(dt)
@@ -34,12 +65,14 @@ end
 function love.draw()
   mod.drawDebug()
   for i,b in ipairs(buttons) do
-    if b.text == "Quit" or b.action == "delete" or b.action == "load" or b.action == "save" or b.action == "clear" then
+    -- set colour for save/load/delete/quit/clear buttons
+    if b.action == "quit" or b.action == "delete" or b.action == "load" or b.action == "save" or b.action == "clear" then
       love.graphics.setColor(1,0.7,1,1)
       love.graphics.rectangle("fill", b.x, b.y, b.width, b.height)
       love.graphics.setColor(1,1,1,1)
       love.graphics.print(b.text, b.x+b.width+5, b.y+b.height, nil, 2, 2, b.width/2, b.height/2)
     else
+      -- set colour for animal buttons
       love.graphics.setColor(1,0.7,.6,1)
       love.graphics.rectangle("fill", b.x, b.y, b.width, b.height)
       love.graphics.setColor(1,1,1,1)
